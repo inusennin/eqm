025d8e405719cfad95c5647ce8d9e0cb685b1f01
William Goodwin <william.goodwin@optanix.com>
2022-07-05 10:45:11 -0400

    Added implementation of single event queue

---
+++ single_event.go
@@ -0,0 +1,48 @@
+package eqm
+
+import "fmt"
+
+type SingleEventQueue struct {
+    Name    string
+    Events  EventChannel
+    Call    func(Event) EventReply
+}
+
+func (seq SingleEventQueue) StartQueue() {
+    for {
+        e := <-seq.Events
+        e.Reply<-seq.Call(e)
+    }
+} 
+
+func  (seq SingleEventQueue) SendEvent(e Event) EventReply {
+    if seq.Call == nil {
+        return NewEventReply("", fmt.Errorf("eventqueue: Call for SiingleEventQueue [%s] not initialized.", seq.Name))
+    }
+
+    if seq.Events == nil {
+        return NewEventReply("", fmt.Errorf("eventqueue: Queue of name [%s] not initialized.", seq.Name))
+    }
+
+    if e.Reply == nil {
+        e.Reply = make(chan EventReply)
+    }
+
+    seq.Events <-e
+    return <-e.Reply
+}
+
+func (seq *SingleEventQueue) SetEventsBuffer(buffSize int) {
+    old := *seq
+
+    old.Events = make(chan Event, buffSize)
+
+    *seq = old
+}
+
+func NewSingleEventQueue(name string) SingleEventQueue {
+    return SingleEventQueue {
+        Name:   name,
+        Events: make(EventChannel),
+    }
+}
